dc2aec03e26d4964c48f8a4204771ac2135a3243
Uzoma <cindyphilips27@gmail.com>
2025-08-25 19:04:13 +0100

    upload sql files

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,26 @@
+/* 
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+- Focuses on job postings with specified salaries (rmeove nulls).
+- BONUS: Include company names of top-paying job postings
+- Why? Highlight the top-paying oppourtunites for Data Analysts, offering insights into employment oppourtunites 
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim USING(company_id)
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL 
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
